2c3bc22bcb7d55421ce48343d516a612b61e2c34
Mücahit Mustafa Usta <mmustafausta@yahoo.com>
2023-04-12 12:39:40 +0300

    feat: implement adding todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import { useRef, useState } from 'react';
+
 function App() {
+  const [todos, setTodos] = useState([]);
+  const inputRef = useRef(null);
+  const handleAddTodoClick = () => {
+    const newTodo = inputRef.current.value;
+    setTodos(todos.concat(newTodo));
+  };
   return (
     <div className="app">
-      Not Empty Projects
+      <div>
+        <label htmlFor="todo-title">Todo Title: </label>
+        <input id="todo-title" ref={inputRef} />
+      </div>
+      <button onClick={handleAddTodoClick} type="button">
+        Add Todo
+      </button>
     </div>
   );
 }
