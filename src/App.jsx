0d9afdf29fcce6b255781785b5eb2ff3da209a01
Mücahit Mustafa Usta <mmustafausta@yahoo.com>
2023-04-16 15:06:33 +0300

    feat: add editable button

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,26 @@
 import { useRef, useState } from 'react';
 
 function App() {
+  const [isEditableList, setIsEditableList] = useState([]);
   const [todos, setTodos] = useState([]);
   const inputRef = useRef(null);
   const handleAddTodoClick = () => {
     const newTodo = inputRef.current.value;
     setTodos(todos.concat(newTodo));
     inputRef.current.value = '';
+    setIsEditableList(isEditableList.concat(false));
   };
   const handleDeleteClick = (index) => {
-    const newTodos = todos.filter((_, todoIndex) => todoIndex !== index);
+    const newTodos = [...todos.slice(0, index), ...todos.slice(index + 1)];
     setTodos(newTodos);
+    const newBooleans = [...isEditableList.slice(0, index), ...isEditableList.slice(index + 1)];
+    setIsEditableList(newBooleans);
   };
-
+  const handleEditClick = (index) => {
+    const newBooleans = [...isEditableList.slice(0, index),
+      !isEditableList[index], ...isEditableList.slice(index + 1)];
+    setIsEditableList(newBooleans);
+  };
   return (
     <div className="app">
       <div>
@@ -26,8 +34,9 @@ function App() {
       <ul>
         {todos.map((todo, index) => (
           <li key={todo}>
-            {todo}
+            {isEditableList[index] ? <input /> : <span>{todo}</span> }
             <button type="button" onClick={() => handleDeleteClick(index)}>Delete Todo</button>
+            <button type="button" onClick={() => handleEditClick(index)}>Edit Todo</button>
           </li>
         ))}
       </ul>
