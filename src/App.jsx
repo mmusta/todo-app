ef2e2abe1e190643e66e8d8539878b900c631ba2
Mücahit Mustafa Usta <mmustafausta@yahoo.com>
2023-04-19 21:50:23 +0300

    feat: add checkbox and update todo types

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import { useRef, useState } from 'react';
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
 
 function App() {
   const [isEditableList, setIsEditableList] = useState([]);
+  /** @type {[{id: string, title: string, isCompleted: boolean}[],]} */
   const [todos, setTodos] = useState([]);
-  const inputRef = useRef(null);
-  const handleAddTodoClick = () => {
-    const newTodo = inputRef.current.value;
-    setTodos(todos.concat(newTodo));
-    inputRef.current.value = '';
-    setIsEditableList(isEditableList.concat(false));
-  };
   const handleDeleteClick = (index) => {
     const newTodos = [...todos.slice(0, index), ...todos.slice(index + 1)];
     setTodos(newTodos);
@@ -22,19 +17,48 @@ function App() {
       !isEditableList[index], ...isEditableList.slice(index + 1)];
     setIsEditableList(newBooleans);
   };
+
+  const handleCheckboxChange = (currentIndex) => {
+    const updatedTodos = todos.map((todo, index) => {
+      if (index === currentIndex) {
+        return {
+          ...todo,
+          isCompleted: !todo.isCompleted,
+        };
+      }
+      return todo;
+    });
+
+    setTodos(updatedTodos);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const newTodo = {
+      id: nanoid(),
+      title: event.target[0].value,
+      isCompleted: false,
+    };
+    setTodos(todos.concat(newTodo));
+    event.target[0].value = '';
+    setIsEditableList(isEditableList.concat(false));
+  };
   return (
     <div className="app">
-      <div>
-        <label htmlFor="todo-title">Todo Title: </label>
-        <input id="todo-title" ref={inputRef} />
-      </div>
-      <button onClick={handleAddTodoClick} type="button">
-        Add Todo
-      </button>
+      <form onSubmit={onSubmit}>
+        <div>
+          <label htmlFor="todo-title">Todo Title: </label>
+          <input id="todo-title" />
+        </div>
+        <button type="submit">
+          Add Todo
+        </button>
+      </form>
       <ul>
         {todos.map((todo, index) => (
-          <li key={todo}>
-            {isEditableList[index] ? <input /> : <span>{todo}</span> }
+          <li key={todo.id}>
+            <input type="checkbox" checked={todo.isCompleted} onChange={() => handleCheckboxChange(index)} />
+            {isEditableList[index] ? <input /> : <span>{todo.title}</span> }
             <button type="button" onClick={() => handleDeleteClick(index)}>Delete Todo</button>
             <button type="button" onClick={() => handleEditClick(index)}>Edit Todo</button>
           </li>
